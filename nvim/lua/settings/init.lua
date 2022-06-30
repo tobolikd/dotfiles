bd8783ead4f197c4b7c932de5f6fe8f6583197be
David Tobolik <tobolikg@gmail.com>
2022-06-30 22:59:30 +0200

    feat: add pure lua config
     - loading packer
     - basic setings
     - prepared modules to be added

---
+++ nvim/lua/settings/init.lua
@@ -0,0 +1,53 @@
+local set = vim.opt
+
+set.fileencoding = 'utf-8'
+
+-- tab options
+set.tabstop = 4
+set.softtabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.smartindent = true
+
+-- sound
+set.errorbells = false
+
+-- terminal
+set.termguicolors = true
+
+-- line numbers
+set.relativenumber = true
+set.number = true
+
+-- cursor to block
+set.guicursor = ''
+set.mouse = 'a'
+
+-- text wrap
+set.wrap = false
+
+-- search
+set.hlsearch = false
+set.smartcase = true
+set.ignorecase = true
+set.incsearch = true
+
+-- history
+set.swapfile = false
+set.hidden = true
+    -- set.nobackup
+    -- set.undodir=~/.vim/undodir
+    -- set.undofile
+
+-- navigation
+set.scrolloff = 8
+set.splitbelow = true
+set.splitright = true
+
+-- err mesage space
+set.cmdheight = 2
+
+-- info columns
+set.signcolumn = 'yes'
+set.colorcolumn = '80'
+
