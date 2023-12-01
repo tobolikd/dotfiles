3e328c279025ab96068fd517b36c7420ef594def
David Tobolik <tobolikg@gmail.com>
2023-12-01 20:19:18 +0100

    nvim: find symbols and references in worskpace

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -5,9 +5,13 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = ' '
 
 map('n', '<leader>e', ':NvimTreeToggle <CR>', opts)
+map('n', 'j', 'gj', opts)
+map('n', 'k', 'gk', opts)
 
 local telescope = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
+vim.keymap.set('n', '<leader>ff', telescope.find_files, opts)
+vim.keymap.set('n', '<leader>fg', telescope.live_grep, opts)
+vim.keymap.set('n', '<leader>fr', telescope.lsp_references, opts)
+vim.keymap.set('n', '<leader>fs', telescope.lsp_dynamic_workspace_symbols, opts)
+vim.keymap.set('n', '<leader>fb', telescope.buffers, opts)
+vim.keymap.set('n', '<leader>fh', telescope.help_tags, opts)
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,4 +6,6 @@ require('telescope').setup {
         }
     }
 }
+
 require('telescope').load_extension('fzy_native')
+
